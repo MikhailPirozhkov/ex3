39296e7ced3f6a02fe06f604d75e7c0785079146
Mikhail Pirozhkov <mipiyu@gmail.com>
2016-02-10 17:35:31 +0300

    version to time 17-35

--- model.php
+++ model.php
@@ -1,3 +1,9 @@
 <?php
 // where we take information about our city - Yaroslavl
 // http://export.yandex.ru/weather-ng/forecasts/27331.xml
+
+class Weather 
+{
+	private  $url;
+	$xml_parser = xml_parser_create();
+}
