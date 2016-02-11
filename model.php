a9b46402363d86f06835d0c76927eef8c9d3bb8e
Mikhail Pirozhkov <mipiyu@gmail.com>
2016-02-11 10:41:48 +0300

    create class Wether

--- model.php
+++ model.php
@@ -4,6 +4,33 @@
 
 class Weather 
 {
-	private  $url;
-	$xml_parser = xml_parser_create();
+	// адрес xml файла
+	private  $data_file;
+	
+	public function __construct($data_file)
+	{
+		$xml = simplexml_load_file($data_file); // раскладываем xml на массив
+	}
+	
+	// выбираем требуемые параметры (город, температура, пиктограмма и тип погоды текстом (облачно, ясно)
+	public function getCity ()
+	{
+		$city=$xml->fact->station;
+		return $city;
+	}
+	public function getTemp ()
+	{
+		$temp=$xml->fact->temperature;
+		// Если значение температуры положительно, для наглядности добавляем "+"
+		if ($temp>0) {$temp='+'.$temp;}
+		return $temp;
+	}
+	public function getImage ()
+	{
+		return $xml->fact->image;
+	}
+	public function getType ()
+	{
+		return $xml->fact->weather_type;
+	}
 }
